6ec294b04c2717ef966bb60a9f43522978e6a2f1
Marcos <77155241+MarckGonNar99@users.noreply.github.com>
2023-02-15 10:52:05 +0100

    Últimos datos
    Viene con ejemplos para borrar y modificar por parte del admin

--- base_datos/web_cocina.sql
+++ base_datos/web_cocina.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-02-2023 a las 17:39:26
+-- Tiempo de generación: 15-02-2023 a las 10:50:48
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 8.0.25
 
@@ -28,6 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `menu` (
+  `id_menu` int(3) NOT NULL,
   `id_user` int(2) NOT NULL,
   `lunes` int(3) NOT NULL,
   `martes` int(3) NOT NULL,
@@ -38,14 +39,6 @@ CREATE TABLE `menu` (
   `domingo` int(3) NOT NULL,
   `fecha` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `menu`
---
-
-INSERT INTO `menu` (`id_user`, `lunes`, `martes`, `miercoles`, `jueves`, `viernes`, `sabado`, `domingo`, `fecha`) VALUES
-(3, 4, 3, 5, 13, 1, 11, 14, '2023-02-06'),
-(8, 2, 16, 4, 5, 14, 13, 1, '2023-02-07');
 
 -- --------------------------------------------------------
 
@@ -76,12 +69,14 @@ INSERT INTO `receta` (`id_receta`, `id_user`, `nombre`, `imagen`, `tiempo`, `cat
 (2, 3, 'Filetes empanados', 'imagenes/recetas/Filetesempanados_3.jpg', 30, 'Carnes y aves', '1kg de carne (pollo o cerdo), 3 huevos, pan rallado, sal, pimienta, perejil, aceite para freir', NULL, 'Cortar la carne en filetes finos. Colocar el pan rallado y los huevos en cuencos distintos, batir los huevos. Sazonar la carne con la sal perejil y pimienta. Pasar los filetes por el cuenco con huevo y después rebozarlo con el pan rallado. Preparar una sartén u olla con el aceite y calentar a fuego alto. Freír los filetes a fuego medio-alto durante 1 min a cada lado, en grupos pequeños para evitar enfriar el aceite. Al sacarlos, colocarlos en una bandeja con papel de cocina para absorber el acei', '2023-01-19', 0),
 (3, 3, 'Pasta con tomate', 'imagenes/recetas/Pastacontomate_3.jpg', 40, 'Pastas', 'pasta seca, bote de tomate triturado, sal, aceite, queso rallado', NULL, 'Preparar una sartén con aceite para freír el tomate. Echar todo el tomate de la lata con un culito de agua. Salar la salsa al gusto y poner fuego medio-bajo, freír a fuego medio-bajo durante 40 minutos, moviendo de vez en cuando para que no se pegue. Cocer la pasta en una olla de agua hirviendo hasta que esté al dente. Colar la pasta, echar queso una vez servido', '2023-01-24', 0),
 (4, 3, 'Ensalada de judias verdes', 'imagenes/recetas/Ensaladadejudiasverdes_3.jpg', 40, 'Verduras y legumbres', 'Judias verdes, media cebolla, patatas (por número de personas), huevo (por número de personas), sal, perejil, aceite, ajo', '', 'Pelar las patatas y trocear las judías, lavarlas con agua y depositar en una olla con agua. Cocer las judías y patatas hasta que estén blandas. Picar la cebolla o dejarla en aritos (lo que más guste) y picar los ajos. Preparar el aliño en un mortero, empezando con el ajo picado, sal y perejil. Echar a la mezcla el aceite y seguir moviendo hasta que se homogenice. Cocer los huevos hasta que estén duros (10 min). Servir las judías y la patata como base, echar la cebolla picada y mezclar, antes de servir aliñar con la mezcla del mortero', '2023-01-30', 0),
-(5, 3, 'Salmón asado', 'imagenes/recetas/Salmónasado_3.jpg', 20, 'Pescado y mariscos', '2 filetes de salmón, aceite, perejil, salsa mostaza con grano, sal', NULL, 'Preparar bandeja de horno con papel vegetal, con un pincel recubrir una capa de aceite donde se ponga el salmón. Poner el salmón en la bandeja y pintar con el pincel sobre ellos la salsa mostaza. Luego esparcir la sal y el perejil al gusto. Hornear 15 minutos a 175 ºC fuego arriba y abajo con ventilación', '2023-01-24', 0),
-(11, 8, 'Pollo al vino', 'imagenes/recetas/Polloalvino_11.png', 90, 'Carnes y aves', 'gfsgfsg', NULL, 'gfsfgfsgs', '2023-01-24', 0),
-(12, 8, 'Ajoblanco', 'imagenes/recetas/Ajoblanco_12.png', 30, 'Sopas, cremas y cocidos', 'fdafdafda', NULL, 'fdsafdsafaf', '2023-01-24', 0),
-(13, 5, 'Sopa de cebolla', 'imagenes/recetas/Sopadecebolla_13.png', 60, 'Sopas, cremas y cocidos', '600d de cebollas, 70g de aceite, 1 cucharada de harina, 100g de queso rallado, 15 rebanadas finas de pan, pimienta blanca molida, sal', NULL, 'Pelar y picar las cebollas; rehogarlas en la satén a fuego lento hasta que estén transparentes y blandas pero sin dorarse. Sazonar con sal y pimienta. Cuando estén casi desechas, agregar la harina y remover hasta obtener una mezcla dorada. Poner esta preparación en una cacerola y verter un cuarto de litro de agua fría. Dejar cocer 40 min a fuego medio-suave. Freír las rebanadas de pan con aceite; escurrirlas y reservarlas. Calentar el horno a temperatura media-alta. Preparar cuencos individuales', '2023-01-30', 0),
+(5, 3, 'Salmón asado', 'imagenes/recetas/Salmónasado_3.jpg', 20, 'Pescado y mariscos', '2 filetes de salmón, aceite, perejil, salsa mostaza con grano, sal', NULL, 'Preparar bandeja de horno con papel vegetal, con un pincel recubrir una capa de aceite donde se ponga el salmón. Poner el salmón en la bandeja y pintar con el pincel sobre ellos la salsa mostaza. Luego esparcir la sal y el perejil al gusto. Hornear 15 minutos a 175 ºC fuego arriba y abajo con ventilación', '2023-02-15', 0),
+(11, 8, 'Pollo al vino', 'imagenes/recetas/Polloalvino_11.png', 90, 'Carnes y aves', '1 pollo de 1500g, sal, 1 cucharada de salsa de tomate, 1 diente de ajo, medio cubito de caldo de pollo, 1 cebolla, aceite, tomillo, 400ml de vino tinto, laurel, 100g de panceta ahumada, 12 cebollas francesas, 175g de champiñones, 4 cucharadas de coñac', NULL, 'Cuartear el pollo y reservar. Poner los menudillos en un cazo de agua; añadir el caldo, la salsa de tomate y la cebolla cortada. Salar y cocer 30 minutos. Escurrir y reservar el liquido. Poner el ajo, pelado y troceado, en un cazo a fuego medio 15 min. Pelar las cebollas, dorarlas en aceite, junto a la panceta en tiras. Sazonar el pollo con la piel hacia abajo y freírlo 2 minutos. Ponerlo en una cazuela, flamear coñac y añadir la panceta y las cebollitas. Colar el vino sobre el pollo, añadir caldo, tapar y hervir 40 min. Agregar los champiñones; sacar el pollo y colocarlo junto a las cebollitas y la panceta. Servir', '2023-02-15', 0),
+(13, 5, 'Sopa de cebolla', 'imagenes/recetas/Sopadecebolla_13.png', 60, 'Sopas, cremas y cocidos', '600d de cebollas, 70g de aceite, 1 cucharada de harina, 100g de queso rallado, 15 rebanadas finas de pan, pimienta blanca molida, sal', NULL, 'Pelar y picar las cebollas; rehogarlas en la satén a fuego lento hasta que estén transparentes y blandas pero sin dorarse. Sazonar con sal y pimienta. Cuando estén casi desechas, agregar la harina y remover hasta obtener una mezcla dorada. Poner esta preparación en una cacerola y verter un cuarto de litro de agua fría. Dejar cocer 40 min a fuego medio-suave. Freír las rebanadas de pan con aceite; escurrirlas y reservarlas. Calentar el horno a temperatura media-alta. Preparar cuencos individuales', '2023-01-30', 1),
 (14, 5, 'Crema de mejillones', 'imagenes/recetas/Cremademejillones_14.png', 60, 'Sopas, cremas y cocidos', '1kg de mejillones, 1 bote de leche evaporada, 2 yemas de huevo, 1 cucharada de Ketchup, Unas gotas de salsa Perrins, 40g de mantequilla, 2 cucharadas de harina, sal y pimienta', 'Mejillones', 'Limpiar los mejillones quitando las barbas, raspando con un cuchillo. Ponerlos en una cacerola al fuego con un vaso de agua hasta que se abran. En un cazo de fondo grueso; fundir la mantequilla y rehogar un 1min la harina. Separar del fuego y añadir la leche mezclada con igual cantidad de agua. Revolver y acercar al fuego. Llevar a ebullición sin dejar de remover y cocer 5min. Debe quedar como una bechamel clara. Sacar los mejillones de sus conchas y reservarlos. Colar el agua de cocerlos y añadirla a la crema con el kétchup y la salsa Perrins. Salpimentar y se añade agua si está muy espesa la mezcla. Retirar la cacerola del fuego y añadir las yemas, batiéndolo. Verterla muy caliente en sopera, añadir los mejillones y servir', '2023-01-30', 0),
-(16, 5, 'Albóndigas en salsa', 'imagenes/recetas/Albóndigasensalsa_16.png', 100, 'Carnes y aves', '600g de carne de ternera picada, 2 huevos, 1 zanahoria, 1 cebolla pequeña, 30g de piñones, 50g de pasas sin pepitas, 5 cucharadas de pan rallado, 2 cucharadas de harina, aceite, 1/2L de caldo de carne, 1 vaso de vino blanco, mostaza, sal y pimienta', NULL, 'Colocar la carne en una hondilla y añadirle los huevos batidos, la cebolla, perejil, zanahoria muy picada, sal, los piñoes y las pasas previamente remojadas una hora en agua. Añadir 3 cucharadas de pan rallado para hacer una masa bien mezclada. Formar las albóndigas, pasarlas por el pan rallado y freírlas en el aceite puesto en una sartén al fuego. Colocarlas en una cacerola con un vaso de agua y 1/2 de vino blanco. Cocer 20 min y guardar al calor. Colocar aceite en una sartén e incorporar la harina. Remover hasta conseguir un color dorado. Agregar el caldo, el vino y dejar cocer 15 min. Sazonar al gusto y al final añadir una cucharadita de mostaza', '2023-01-30', 0);
+(16, 5, 'Albóndigas en salsa', 'imagenes/recetas/Albóndigasensalsa_16.png', 100, 'Carnes y aves', '600g de carne de ternera picada, 2 huevos, 1 zanahoria, 1 cebolla pequeña, 30g de piñones, 50g de pasas sin pepitas, 5 cucharadas de pan rallado, 2 cucharadas de harina, aceite, 1/2L de caldo de carne, 1 vaso de vino blanco, mostaza, sal y pimienta', NULL, 'Colocar la carne en una hondilla y añadirle los huevos batidos, la cebolla, perejil, zanahoria muy picada, sal, los piñoes y las pasas previamente remojadas una hora en agua. Añadir 3 cucharadas de pan rallado para hacer una masa bien mezclada. Formar las albóndigas, pasarlas por el pan rallado y freírlas en el aceite puesto en una sartén al fuego. Colocarlas en una cacerola con un vaso de agua y 1/2 de vino blanco. Cocer 20 min y guardar al calor. Colocar aceite en una sartén e incorporar la harina. Remover hasta conseguir un color dorado. Agregar el caldo, el vino y dejar cocer 15 min. Sazonar al gusto y al final añadir una cucharadita de mostaza', '2023-01-30', 1),
+(19, 4, 'Pollo asado', 'imagenes/recetas/Polloasado_4.jpg', 80, 'Carnes y aves', '1 pollo de 1200g a 1400g,\r\n3 lonchas de bacon finas,\r\n30g de manteca de cerdo,\r\n1 copita de vino blanco,\r\nsal', NULL, 'Limpiar t chamuscar el pollo; depojarlo del cuello y las patas amarillas.\r\nUntar con la manteca de cerdo bien repartida por la piel.\r\nSalar por dentro y fuera.\r\nAtarle 2 lonchas de bacon, una en la pechuga y otra en la espalda.\r\nPoner el pollo e una fuente de horno, calentar 10 min este y meter el pollo, asandolo a calor moderado media hora, dandole vueltas y regando con su jugo.\r\nAl terminar la media hora, sacarlo y rociarle el vino blanco y dejarlo asar otra media hora.\r\nYa tierno, despojarlo del hilo y el bacon (se tira) y reservarlo.\r\nQuitar la grasa de la salsa, añadir un poco de agua caliente.\r\nServir el pollo trinchado', '2023-02-15', 0),
+(20, 3, 'rsgfsgfs', 'imagenes/recetas/rsgfsgfs_3.jpg', 23, 'Carnes y aves', 'gfgfgsgfsgf', NULL, 'gfsgsgfsfsgfsgfs', '2023-02-15', 0),
+(21, 4, 'Coliflor con bechamel', 'imagenes/recetas/Coliflorconbechamel_21.png', 60, 'Verduras y legumbres', '1 coliflor grande, 100g de mantequilla, 50 g de harina, 1/2L de leche, queso rallado, nuez moscada, sal y pimienta', 'Quso', 'Trocear la coliflor y cocerla en agua con sal; escurrir y reservar. Fundir la mantequilla en un cazo, añadir la harina y mover; añadir poco a poco la leche caliente, batiendo con varillas para no formar grumos, sazonar con sal, pimienta y nuez moscada. Poner la coliflor bien escurrida en una fuente de horno, cubrir con la bechamel y espolvorear el queso rallado. Gratinar unos minutos a horno fuerte y scar cuando esté bien dorada toda la superficie. Servir muy caliente', '2023-02-15', 0);
 
 -- --------------------------------------------------------
 
@@ -96,6 +91,14 @@ CREATE TABLE `reporte` (
   `fecha` date NOT NULL,
   `motivo` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `reporte`
+--
+
+INSERT INTO `reporte` (`id_reporte`, `id_user`, `id_receta`, `fecha`, `motivo`) VALUES
+(4, 4, 20, '2023-02-15', 'Esto no es contenido de cocina. AVISO'),
+(5, 3, 21, '2023-02-15', 'Pones Quso en vez de queso, alguien que pueda corregirlo');
 
 -- --------------------------------------------------------
 
@@ -117,7 +120,7 @@ CREATE TABLE `usuario` (
 INSERT INTO `usuario` (`id_user`, `nombre`, `contraseña`, `foto`) VALUES
 (1, 'Admin', 'c5e3539121c4944f2bbe097b425ee774', NULL),
 (3, 'marcos', 'c5e3539121c4944f2bbe097b425ee774', '../imagenes/usuarios/Marcos1199_3.jpg'),
-(4, 'carlos', 'dc599a9972fde3045dab59dbd1ae170b', NULL),
+(4, 'carlos', 'dc599a9972fde3045dab59dbd1ae170b', '../imagenes/usuarios/carlos_4.jpg'),
 (5, 'paloma', 'a20df7f02202e665e6a7b674bbfb1fcc', '../imagenes/usuarios/paloma_5.jpg'),
 (6, 'laura', '680e89809965ec41e64dc7e447f175ab', NULL),
 (7, 'miguel', '9eb0c9605dc81a68731f61b3e0838937', NULL),
@@ -137,6 +140,14 @@ CREATE TABLE `votacion` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Volcado de datos para la tabla `votacion`
+--
+
+INSERT INTO `votacion` (`id_voto`, `id_user`, `id_receta`) VALUES
+(16, 3, 16),
+(15, 8, 13);
+
+--
 -- Índices para tablas volcadas
 --
 
@@ -144,14 +155,15 @@ CREATE TABLE `votacion` (
 -- Indices de la tabla `menu`
 --
 ALTER TABLE `menu`
-  ADD PRIMARY KEY (`id_user`),
+  ADD PRIMARY KEY (`id_menu`),
   ADD KEY `lunes` (`lunes`),
   ADD KEY `martes` (`martes`),
   ADD KEY `miercoles` (`miercoles`),
   ADD KEY `jueves` (`jueves`),
   ADD KEY `viernes` (`viernes`),
   ADD KEY `sabado` (`sabado`),
-  ADD KEY `domingo` (`domingo`);
+  ADD KEY `domingo` (`domingo`),
+  ADD KEY `id_user` (`id_user`);
 
 --
 -- Indices de la tabla `receta`
@@ -187,16 +199,22 @@ ALTER TABLE `votacion`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `id_menu` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT de la tabla `receta`
 --
 ALTER TABLE `receta`
-  MODIFY `id_receta` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id_receta` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT de la tabla `reporte`
 --
 ALTER TABLE `reporte`
-  MODIFY `id_reporte` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_reporte` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
@@ -208,7 +226,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `votacion`
 --
 ALTER TABLE `votacion`
-  MODIFY `id_voto` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_voto` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Restricciones para tablas volcadas
